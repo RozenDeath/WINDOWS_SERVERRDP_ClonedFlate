57b467035c019ce75bcd56e61240a4e10cd4f952
Fiate <83636534+Fiate@users.noreply.github.com>
2021-05-04 14:14:44 +0200

    Update lab6cfree.bat

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -2,17 +2,17 @@ tzutil /s "GMT Standard Time"
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f
-certutil -urlcache -split -f https://gitlab.com/haithuongpro2711/haivps/-/raw/master/curl.exe
-certutil -urlcache -split -f https://gitlab.com/haithuongpro2711/haivps/-/raw/master/7z.dll
-certutil -urlcache -split -f https://gitlab.com/haithuongpro2711/haivps/-/raw/master/7z.exe
-certutil -urlcache -split -f https://gitlab.com/haithuongpro2711/haivps/-/raw/master/nircmd.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/curl.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/7z.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/7z.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 
 
 cd C:\Users\Administrator\Desktop
-curl -LJOk https://gitlab.com/haithuongpro2711/haivps/-/raw/master/ChromeSetup.exe
-curl -LJOk https://gitlab.com/haithuongpro2711/haivps/-/raw/master/anti-close.bat
+curl -LJOk https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/ChromeSetup.exe
+curl -LJOk https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/anti-close.bat
 start ChromeSetup.exe
 cls
 @echo off

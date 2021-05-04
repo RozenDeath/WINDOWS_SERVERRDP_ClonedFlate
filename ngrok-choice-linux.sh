acc96d1c6f8f9100bc71d22fbb23b79847f54d4d
Fiate <83636534+Fiate@users.noreply.github.com>
2021-05-04 14:16:17 +0200

    Update ngrok-choice-linux.sh

--- ngrok-choice-linux.sh
+++ ngrok-choice-linux.sh
@@ -4,13 +4,13 @@ foods=("US" "AP")
 select fav in "${foods[@]}"; do
     case $fav in
         "US")
-            wget https://gitlab.com/haithuongpro2711/haivps/-/raw/master/ngrok-us-linux.sh
+            wget https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/ngrok-us-linux.sh
             chmod +x ngrok-us-linux.sh    
 	    ./ngrok-us-linux.sh 33899
             break
             ;;
         "AP")
-            wget https://gitlab.com/haithuongpro2711/haivps/-/raw/master/ngrok-ap-linux.sh
+            wget https://raw.githubusercontent.com/Fiate/WINDOWSRDP/main/ngrok-ap-linux.sh
             chmod +x ngrok-ap-linux.sh    
 	    ./ngrok-ap-linux.sh 33899
             break
